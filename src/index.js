862b69a1dc482af5072d70b92d5bd399bca2fe87
Guandjoy <guandjoy@gmail.com>
2019-04-27 17:06:35 +0300

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Notes from "./Notes";
 import "./styles.css";
-
-const initItems = [
-  { id: 1, content: "one", order: 0, x: 0, y: 0 },
-  { id: 2, content: "two", order: 1, x: 112, y: 0 },
-  { id: 3, content: "three", order: 2, x: 224, y: 0 }
-];
 
 function App() {
   return (
