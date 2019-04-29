455c8db8d3856531003dc72350b5001027acc065
Guandjoy <guandjoy@gmail.com>
2019-04-29 21:25:43 +0300

    Refactor and debug

--- src/Notes.js
+++ src/Notes.js
@@ -3,22 +3,34 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 const initItems = [
-  { id: 1, content: "one", order: 0, x: 0 + 15, y: 0 + 15 },
-  { id: 2, content: "two", order: 1, x: 100 + 30, y: 0 + 15 },
-  { id: 3, content: "three", order: 2, x: 200 + 45, y: 0 + 15 }
+  { id: 1, content: "one", order: 0 },
+  { id: 2, content: "two", order: 1 },
+  { id: 3, content: "three", order: 2 }
 ];
 
+// Items positioning methods
+const getXPos = item => {
+  return `${item.order * 100 + 8 * item.order + 8}px`;
+};
+
+const getYPos = item => {
+  return `8px`;
+};
+
 function Notes() {
+  // General
+  const [items, setItems] = useState(initItems);
+  const [cursorPos, setCursorPos] = useState(undefined); // Pos of mouse or touch
+  const [lastRearrangedItemId, setLastRearrangedItemId] = useState();
+  const [UILog, setUILog] = useState();
+  // Touch events
   const [isTouch, setIsTouch] = useState(false);
-  const [touchPos, setTouchPos] = useState();
-  const [touchOver, setTouchOver] = useState();
   const [touchStatus, setTouchStatus] = useState("not active");
+  const [touchOverId, setTouchOverId] = useState(undefined);
+  // Drag events
   const [dndStatus, setDndStatus] = useState("not active");
   const [dragItem, setDragItem] = useState();
-  const [lastOverItem, setLastOverItem] = useState();
   const [dragPoint, setDragPoint] = useState({ x: 0, y: 0 });
-  const [mousePos, setMousePos] = useState(undefined);
-  const [items, setItems] = useState(initItems);
 
   useEffect(() => {
     document.addEventListener("dragover", onDragOverSpace);
@@ -29,8 +41,7 @@ function Notes() {
     e.preventDefault && e.preventDefault();
     setTouchStatus("touch start");
     setIsTouch(true);
-    setTouchPos({ x: e.touches[0].clientX, y: e.touches[0].clientY });
-    setMousePos({ x: e.touches[0].clientX, y: e.touches[0].clientY });
+    setCursorPos({ x: e.touches[0].clientX, y: e.touches[0].clientY });
     let fingers = e.touches.length;
     let pos = { x: e.touches[0].clientX, y: e.touches[0].clientY };
     setTouchStatus(
@@ -40,11 +51,16 @@ function Notes() {
 
   const onTouchMove = e => {
     e.preventDefault && e.preventDefault();
+    setTouchStatus("touch move");
+    setCursorPos({ x: e.touches[0].clientX, y: e.touches[0].clientY });
     let overObjectId = document.elementFromPoint(
       e.touches[0].clientX,
       e.touches[0].clientY
     ).id;
-    if (overObjectId && dragItem) {
+    setTouchOverId(overObjectId);
+    setUILog("Note Rearranging");
+    if (overObjectId && dragItem && lastRearrangedItemId !== overObjectId) {
+      setUILog("rearranging");
       items.forEach(item => {
         if (Object.values(item).indexOf(overObjectId) > -1) {
           overObjectId = Object.values(item).indexOf(overObjectId);
@@ -52,12 +68,6 @@ function Notes() {
       });
       rearrangeItems(items[overObjectId - 1]);
     }
-
-    setTouchOver(overObjectId);
-    setTouchPos({ x: e.touches[0].clientX, y: e.touches[0].clientY });
-    dragItem &&
-      setMousePos({ x: e.touches[0].clientX, y: e.touches[0].clientY });
-    setTouchStatus("touch move");
   };
 
   const onTouchEnd = e => {
@@ -69,86 +79,85 @@ function Notes() {
   const onDragStart = (e, note) => {
     setDndStatus("drag start");
     setDragItem(note);
-    setMousePos({ x: e.clientX, y: e.clientY });
-    let x, y;
-    if (isTouch) {
-      x = touchPos.x - note.x;
-      y = touchPos.y - note.y;
-    } else {
-      x = e.clientX - note.x;
-      y = e.clientY - note.y;
-    }
+
+    const dragElement = document.getElementById(note.id);
+    console.log(e.clientX - (e.clientX - dragElement.offsetLeft));
     setDragPoint({
-      x,
-      y
+      x: (isTouch ? cursorPos.x : e.clientX) - dragElement.offsetLeft,
+      y: (isTouch ? cursorPos.y : e.clientY) - dragElement.offsetTop
     });
+
+    setCursorPos({ x: e.clientX, y: e.clientY });
   };
 
   const rearrangeItems = overItem => {
     var newOrder = [];
     var newItems = undefined;
     setItems(() => {
-      if (overItem !== dragItem && overItem !== lastOverItem) {
+      if (overItem !== dragItem && overItem.id !== lastRearrangedItemId) {
         setDndStatus("drag over");
         items.forEach((item, index) => {
-          // Iterate through each item and assign order
+          newOrder[index] = item.order; // Item is out of range. Keep same order
+          // For item needs to be changed
           if (dragItem.order < overItem.order) {
-            // Drag toward end
-            // keep same order
-            if (item.order < dragItem.order) newOrder[index] = item.order;
-            // Inbetween notes. Replace on one to start
+            // Drag toward the end
             if (item.order > dragItem.order && item.order <= overItem.order)
+              // Inbetween notes. Replace on one to the start
               newOrder[index] = item.order - 1;
-            // Keep same order. Note is out for range
-            if (item.order > overItem.order) newOrder[index] = item.order;
-            // Assign new order to draggable
-            if (item.order === dragItem.order) newOrder[index] = overItem.order;
+            if (item.order === dragItem.order)
+              // Assign new order to the draggable
+              newOrder[index] = overItem.order;
           }
           if (dragItem.order > overItem.order) {
-            // Drag toward start
-            // keep same order
-            if (item.order > dragItem.order) newOrder[index] = item.order;
-            // Inbetween notes. Replace on one to start
+            // Drag toward the start
             if (item.order < dragItem.order && item.order >= overItem.order)
+              // Inbetween notes. Replace on one to the end
               newOrder[index] = item.order + 1;
-            // Keep same order. Note is out for range
-            if (item.order < overItem.order) newOrder[index] = item.order;
-            // Assign new order to draggable
-            if (item.order === dragItem.order) newOrder[index] = overItem.order;
+            if (item.order === dragItem.order)
+              // Assign new order to the draggable
+              newOrder[index] = overItem.order;
           }
         });
         newItems = items.map((item, index) => {
           item.order = newOrder[index];
-          item.x = newOrder[index] * 100 + newOrder[index] * 15 + 15;
-          item.y = 15;
           return item;
         });
       }
       if (newItems) return newItems;
       return items;
     });
-    setLastOverItem(overItem);
+    setLastRearrangedItemId(overItem.id);
   };
 
-  const onDragOver = (e, overItem) => {
+  const onDragOverItem = (e, overItem) => {
     if (e.preventDefault) {
       e.preventDefault(); // Necessary. Allows us to drop.
     }
     rearrangeItems(overItem);
   };
 
+  const onDragOverSpace = e => {
+    setCursorPos({ x: e.clientX, y: e.clientY });
+  };
+
   const onDragEnd = (e, note) => {
     setDndStatus("drag end");
     // Cleanup after dragging
     setDragItem(undefined);
-    setLastOverItem(undefined);
+    setLastRearrangedItemId(undefined);
     setDragPoint(undefined);
-    setMousePos(undefined);
+    setCursorPos(undefined);
   };
 
-  const onDragOverSpace = e => {
-    setMousePos({ x: e.clientX, y: e.clientY });
-  };
+  // Setup ghost on render
+  let ghost;
+  if (dragItem) {
+    ghost = {
+      item: dragItem,
+      x: cursorPos.x - dragPoint.x,
+      y: cursorPos.y - dragPoint.y
+    };
+  }
 
   return (
     <div className="App">
@@ -159,36 +168,38 @@ function Notes() {
           className={`note ${!dragItem && "hoverable"}`}
           key={note.content}
           onDragStart={e => onDragStart(e, note)}
-          onDragOver={e => onDragOver(e, note)}
+          onDragOver={e => onDragOverItem(e, note)}
           onDragEnd={e => onDragEnd(e, note)}
           onTouchStart={onTouchStart}
           onTouchMove={onTouchMove}
           onTouchEnd={onTouchEnd}
           style={{
             opacity: dragItem === note ? 0 : 1,
-            left: `${note.x}px`,
-            top: `${note.y}px`
+            left: getXPos(note),
+            top: getYPos(note)
           }}
         >
           <p>{note.content}</p>
         </div>
       ))}
-      {dragItem && (
+      {ghost && (
         <div
-          className="ghost"
-          style={
-            mousePos && {
-              visibility: "visible",
-              transform: `translate(${mousePos.x -
-                dragPoint.x}px, ${mousePos.y - dragPoint.y}px)`
-            }
-          }
+          className="note ghost"
+          style={{
+            transform: `translate(${ghost.x}px, ${ghost.y}px)`
+          }}
         >
-          <p>{dragItem.content}</p>
+          <p>{ghost.item.content}</p>
         </div>
       )}
+      <h6 style={{ position: "fixed", left: 0, bottom: 100 }}>
+        UI log : {UILog && UILog.toString()}
+      </h6>
+      <h6 style={{ position: "fixed", left: 0, bottom: 80 }}>
+        rearranged item: {lastRearrangedItemId && lastRearrangedItemId}
+      </h6>
       <h6 style={{ position: "fixed", left: 0, bottom: 60 }}>
-        touch over: {touchOver && touchOver}
+        touch over id: {touchOverId}
       </h6>
       <h6 style={{ position: "fixed", left: 0, bottom: 40 }}>
         touch status: {touchStatus}
