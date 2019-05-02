e3706b65fda93778ca047f12602921d1b3b3be7d
Guandjoy <guandjoy@gmail.com>
2019-05-02 20:06:01 +0300

    Add more examples and randomize height

--- src/Notes.js
+++ src/Notes.js
@@ -1,23 +1,44 @@
 import React from "react";
+import Draggable from "./Draggable";
 import DraggableMasonryLayout from "./DraggableMasonryLayout";
 
 import "./styles.css";
+const maxHeight = 200;
+const minHeight = 100;
+const randomHeight = () =>
+  Math.floor(Math.random() * (maxHeight - minHeight) + minHeight);
 
 const nodes = [
-  { id: "1", content: "one" },
-  { id: "2", content: "two" },
-  { id: "3", content: "three" }
+  { id: "1", content: "one", height: randomHeight() },
+  { id: "2", content: "two", height: randomHeight() },
+  { id: "3", content: "three", height: randomHeight() },
+  { id: "4", content: "four", height: randomHeight() },
+  { id: "5", content: "five", height: randomHeight() },
+  { id: "6", content: "six", height: randomHeight() },
+  { id: "7", content: "seven", height: randomHeight() },
+  { id: "8", content: "eight", height: randomHeight() },
+  { id: "9", content: "nine", height: randomHeight() },
+  { id: "10", content: "ten", height: randomHeight() },
+  { id: "11", content: "eleven", height: randomHeight() },
+  { id: "12", content: "twelve", height: randomHeight() },
+  { id: "13", content: "thirteen", height: randomHeight() },
+  { id: "14", content: "fourteen", height: randomHeight() },
+  { id: "15", content: "fifteen", height: randomHeight() },
+  { id: "16", content: "sixteen", height: randomHeight() },
+  { id: "17", content: "seventeen", height: randomHeight() }
 ];
 
 function Notes() {
   const notes = nodes.map((node, index) => (
     <div
       id={node.id}
+      height={node.height}
       index={index}
       order={index}
       draggable="true"
-      className={`note`}
+      className="note"
       key={node.id}
+      style={{ height: `${node.height}px` }}
     >
       <p>{node.content}</p>
     </div>
